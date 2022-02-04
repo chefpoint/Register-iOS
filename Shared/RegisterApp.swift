8f2f8ca5ab0b6b544e22a4fab06b52a590a3ed8d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-02-04 12:53:07 +0000

    Initial Commit

---
+++ Shared/RegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterApp.swift
+//  Shared
+//
+//  Created by João de Vasconcelos on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
